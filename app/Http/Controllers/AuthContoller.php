10d111e2d3f4f6f6de609e7890c194e49a352d19
fagundes321 <victorfagundes123@gmail.com>
2026-02-11 15:40:41 -0300

    feat: Validação de entrada do login

--- app/Http/Controllers/AuthContoller.php
+++ app/Http/Controllers/AuthContoller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthContoller extends Controller
 {
@@ -18,23 +20,52 @@ class AuthContoller extends Controller
     public function loginSubmit(Request $request)
     {
         // form validation
-        $request->validate([
-            'text_username' => 'required|email',
-            'text_password' => 'required|min:6|max:16'
-        ],
-        [
-            'text_username.required' => 'O username é obrigatorio',
-            'text_username.email'    => 'Username deve ser um email válido',
-            'text_password.required'  => 'A senha é obrigatorio',
-            'text_password.min'    => 'A senha deve ter pelo menos :min caracteres',
-            'text_password.max'    => 'A senha deve ter no máximo :max caracteres'
-        ]);
+        $request->validate(
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:16'
+            ],
+            [
+                'text_username.required' => 'O username é obrigatorio',
+                'text_username.email'    => 'Username deve ser um email válido',
+                'text_password.required'  => 'A senha é obrigatorio',
+                'text_password.min'    => 'A senha deve ter pelo menos :min caracteres',
+                'text_password.max'    => 'A senha deve ter no máximo :max caracteres'
+            ]
+
+
+        );
 
         // get user input
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        echo 'OK!';
+        // check if user exists
+        $user = User::where('username', $username)
+            ->where('deleted_at', NULL)
+            ->first();
+
+        if (!$user) {
+            return redirect()->back()->withInput()->with('loginError', 'Username ou Senha incorreta');
+        }
+
+        // check if password is correct
+        if(!password_verify($password, $user->password)){
+              return redirect()->back()->withInput()->with('loginError', 'Username ou Senha incorreta');
+        }
+
+        // update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        session([
+            'user' =>[
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo 'login foi um secesso';
 
     }
 
